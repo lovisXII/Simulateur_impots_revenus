c4370b335a9f7424221314d20f725f4ab165a41c
LovisXII <logeofpit@yahoo.fr>
2024-07-11 22:44:21 +0200

    make both tax array global

--- impots.py
+++ impots.py
@@ -4,29 +4,14 @@ import matplotlib.pyplot as plt
 import numpy as np
 import mplcursors
 
-def old_tax(wage) :
-    tax_to_pay = 0
-    tax_rate = [
-                (0     , 11294  , 0),
-                (11295 , 28797  , 0.11),
-                (28798 , 82341  , 0.30),
-                (82342 , 177106 , 0.41)
-                ]
+old_tax_rate = [
+            (0     , 11294  , 0),
+            (11295 , 28797  , 0.11),
+            (28798 , 82341  , 0.30),
+            (82342 , 177106 , 0.41)
+            ]
 
-    for min_tranch, max_tranch, rate in tax_rate :
-        tax_per_tranch = 0
-        if wage >= max_tranch : # case where wage is higher than the max value of the tranch (ex : wage = 20k should pay 102,92 + 257.3 + 456.2)
-            tax_per_tranch += (max_tranch - min_tranch)*rate
-        elif min_tranch <= wage <= max_tranch :
-            tax_per_tranch += (wage - min_tranch)*rate
-        tax_to_pay += tax_per_tranch
-    if wage > 177106 :
-        tax_to_pay += (wage - 177106) * 0.45
-    return tax_to_pay
-
-def new_tax(wage) :
-    tax_to_pay = 0
-    tax_rate = [
+new_tax_rate = [
                 (0     , 10292  , 0.01),
                 (10292 , 15438  , 0.05),
                 (15438 , 20584  , 0.1),
@@ -42,7 +27,24 @@ def new_tax(wage) :
                 (267592, 411683 , 0.60)
                 ]
 
-    for min_tranch, max_tranch, rate in tax_rate :
+def old_tax(wage) :
+    tax_to_pay = 0
+
+    for min_tranch, max_tranch, rate in old_tax_rate :
+        tax_per_tranch = 0
+        if wage >= max_tranch : # case where wage is higher than the max value of the tranch (ex : wage = 20k should pay 102,92 + 257.3 + 456.2)
+            tax_per_tranch += (max_tranch - min_tranch)*rate
+        elif min_tranch <= wage <= max_tranch :
+            tax_per_tranch += (wage - min_tranch)*rate
+        tax_to_pay += tax_per_tranch
+    if wage > 177106 :
+        tax_to_pay += (wage - 177106) * 0.45
+    return tax_to_pay
+
+def new_tax(wage) :
+    tax_to_pay = 0
+
+    for min_tranch, max_tranch, rate in new_tax_rate :
         tax_per_tranch = 0
         if wage >= max_tranch : # case where wage is higher than the max value of the tranch (ex : wage = 20k should pay 102,92 + 257.3 + 456.2)
             tax_per_tranch += (max_tranch - min_tranch)*rate
@@ -56,8 +58,9 @@ def new_tax(wage) :
 def plot_tax_comparison(wages, new_tax_values, old_tax_values):
     # Plot the data
     fig, ax = plt.subplots()
-    scatter_new = ax.scatter(wages, new_tax_values, label='Systeme de taxation actuel', color='blue')
-    scatter_old = ax.scatter(wages, old_tax_values, label='Systeme de taxation du nfp', color='red')
+    ax.scatter(wages, new_tax_values, label='Systeme de taxation actuel', color='blue')
+    ax.scatter(wages, old_tax_values, label='Systeme de taxation du nfp', color='red')
+    
     ax.set_xlabel('Salaire en euros')
     ax.set_ylabel('Tax en euros')
     ax.legend()
